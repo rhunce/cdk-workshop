ef47c96e52330c84521b46d7213e444c3a94646d
Raymond Hunce <hunce18c@gmail.com>
2024-02-17 18:17:02 -0500

    Create lambda function

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import * as cdk from "aws-cdk-lib";
+import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
+// This is the entrypoint of our application
+// It will load the stack defined in /lib/cdk-workshop-stack
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new CdkWorkshopStack(app, "CdkWorkshopStack");
