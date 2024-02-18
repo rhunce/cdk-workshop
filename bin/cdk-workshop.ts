bb79fcb00be98e84e48c31c7c1fd16c99bb867a6
Raymond Hunce <hunce18c@gmail.com>
2024-02-18 12:57:28 -0500

    Remove WorkshopPipelineStack

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
 // This is the entrypoint of our application
 // It will load the stack defined in /lib/cdk-workshop-stack
 const app = new cdk.App();
-new WorkshopPipelineStack(app, "CdkWorkshopPipelineStack");
+new CdkWorkshopStack(app, "CdkWorkshopStack");
