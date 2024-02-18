8c13c8219f28d5ff6d2162965ec93b6d28161f27
Raymond Hunce <hunce18c@gmail.com>
2024-02-18 11:22:59 -0500

    Add test for HitCounter Construct HitCounterHandler lambda function

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -17,3 +17,32 @@ test("DynamoDB Table Created", () => {
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::DynamoDB::GlobalTable", 1);
 });
+
+test("Lambda Has Environment Variables", () => {
+  const stack = new cdk.Stack();
+  // WHEN
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new lambda.Function(stack, "TestFunction", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    }),
+  });
+  // THEN
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual({
+    Variables: {
+      DOWNSTREAM_FUNCTION_NAME: {
+        Ref: "TestFunction22AD90FC",
+      },
+      HITS_TABLE_NAME: {
+        Ref: "MyTestConstructHits24A357F0",
+      },
+    },
+  });
+});
