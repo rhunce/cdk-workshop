1e6bdc11260d747d3e68771a0bb5c5b015f2dfda
Raymond Hunce <hunce18c@gmail.com>
2024-02-17 21:17:15 -0500

    Grant hitcounter lambda permission to read and write to Hits table

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -30,5 +30,8 @@ export class HitCounter extends Construct {
         HITS_TABLE_NAME: table.tableName,
       },
     });
+
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(this.handler);
   }
 }
