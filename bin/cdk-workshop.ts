3d3f06cbeb48fd7b68da8cc3c3bc2a0695d423b0
Raymond Hunce <hunce18c@gmail.com>
2024-02-18 12:36:32 -0500

    Implement WorkshopPipelineStack

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
+import { WorkshopPipelineStack } from "../lib/pipeline-stack";
 
 // This is the entrypoint of our application
 // It will load the stack defined in /lib/cdk-workshop-stack
 const app = new cdk.App();
-new CdkWorkshopStack(app, "CdkWorkshopStack");
+new WorkshopPipelineStack(app, "CdkWorkshopPipelineStack");
